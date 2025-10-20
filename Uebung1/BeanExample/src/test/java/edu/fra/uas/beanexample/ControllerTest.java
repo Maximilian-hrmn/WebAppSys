796d86c3eb74558dffc58ca5c305b7bc2eb2079f
Maximilian.hrmn <maxi.herman@t-online.de>
2025-10-20 17:22:32 +0200

    test

--- Uebung1/BeanExample/src/test/java/edu/fra/uas/beanexample/ControllerTest.java
+++ Uebung1/BeanExample/src/test/java/edu/fra/uas/beanexample/ControllerTest.java
@@ -1,7 +1,6 @@
-package edu.fra.uas.beanbeispiel;
+package edu.fra.uas.beanexample;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
