b687363dd6c44eb71e59af6249c4d299bfbf6f66
Maximilian.hrmn <maxi.herman@t-online.de>
2025-10-21 17:44:19 +0200

    hausaufgaben initial commit

---
+++ Uebung1/Hausaufgaben/notendurchschnitt/src/test/java/uebung1/notendurchschnitt/NotendurchschnittApplicationTests.java
@@ -0,0 +1,13 @@
+package uebung1.notendurchschnitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotendurchschnittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
